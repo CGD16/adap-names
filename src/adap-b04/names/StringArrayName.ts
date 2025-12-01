48950efa3754db666cfc224aa106db5ff131086b
CGD16 <changgeng.drewes@gmail.com>
2025-12-01 15:03:04 +0100

    Implement preconditions, postconditions, and class invariants

--- src/adap-b04/names/StringArrayName.ts
+++ src/adap-b04/names/StringArrayName.ts
@@ -2,14 +2,57 @@ import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 import { AbstractName } from "./AbstractName";
 
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { MethodFailedException } from "../common/MethodFailedException";
+import { InvalidStateException } from "../common/InvalidStateException";
+
 export class StringArrayName extends AbstractName {
 
     protected components: string[] = [];
 
     constructor(source: string[], delimiter?: string) {
         super();
-        throw new Error("needs implementation or deletion");
+        IllegalArgumentException.assert(source !== null, "source is null");
+        IllegalArgumentException.assert(source !== undefined, "source is undefined");
+
+        if (delimiter !== undefined) {
+            IllegalArgumentException.assert(delimiter !== null, "delimiter is null");
+            IllegalArgumentException.assert(delimiter.length > 0, "delimiter is empty");
+        }
+
+        if (delimiter) {
+            this.delimiter = delimiter;
+        } 
+        if (typeof (source as any) === "string") {
+            this.components = (source as any).split(this.delimiter);
+        } else {
+            this.components = source.slice();
+        }
+
+        for (let i = 0; i < this.components.length; i++) {
+            IllegalArgumentException.assert(this.components[i] !== null, "component is null");
+            IllegalArgumentException.assert(this.components[i] !== undefined, "component is undefined");
+            IllegalArgumentException.assert(this.components[i].length > 0, "component is empty");
+        }
+        this.assertClassInvariant();
+
     }
+
+    protected assertClassInvariant(): void {
+        InvalidStateException.assert(
+            this.components !== null,
+            "components array is null"
+        );
+        InvalidStateException.assert(
+            this.getNoComponents() >= 0,
+            "noComponents is negative"
+        );
+        InvalidStateException.assert(
+            this.delimiter.length > 0,
+            "delimiter is empty"
+        );
+    }
+
 
     public clone(): Name {
         return new StringArrayName(this.components, this.delimiter);
@@ -48,28 +91,68 @@ export class StringArrayName extends AbstractName {
     }
 
     public getComponent(i: number): string {
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i < this.getNoComponents(), "index out of bounds");
+
         return this.components[i];
     }
 
     public setComponent(i: number, c: string) {
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i < this.getNoComponents(), "index out of bounds");
+        IllegalArgumentException.assert(c !== null, "component is null");
+        IllegalArgumentException.assert(c !== undefined, "component is undefined");
+        IllegalArgumentException.assert(c.length > 0, "component is empty");
+
         this.components[i] = c;
+
+        MethodFailedException.assert(this.components[i] === c, "component not set correctly");
+        this.assertClassInvariant();
     }
 
     public insert(i: number, c: string) {
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i <= this.getNoComponents(), "index out of bounds");
+        IllegalArgumentException.assert(c !== null, "component is null");
+        IllegalArgumentException.assert(c !== undefined, "component is undefined");
+        IllegalArgumentException.assert(c.length > 0, "component is empty");
+
         this.components.splice(i, 0, c);
+        this.assertClassInvariant();
     }
 
     public append(c: string) {
+        IllegalArgumentException.assert(c !== null, "component is null");
+        IllegalArgumentException.assert(c !== undefined, "component is undefined");
+        IllegalArgumentException.assert(c.length > 0, "component is empty");
+
         this.components.push(c);
+
+        this.assertClassInvariant();
     }
 
     public remove(i: number) {
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i < this.getNoComponents(), "index out of bounds");
+        IllegalArgumentException.assert(this.getNoComponents() > 0, "cannot remove from empty name");
+
         this.components.splice(i, 1);
+        this.assertClassInvariant();
     }
 
     public concat(other: Name): void {
+        IllegalArgumentException.assert(other !== null, "other name is null");
+        IllegalArgumentException.assert(other !== undefined, "other name is undefined");
+
+        const oldCount = this.getNoComponents(); 
+        const otherCount = other.getNoComponents(); 
+
         for (let i = 0; i < other.getNoComponents(); i++) {
             this.append(other.getComponent(i)); 
         }
+
+        MethodFailedException.assert(this.getNoComponents() === oldCount + otherCount, "concat did not add all components");
+    
+        this.assertClassInvariant();
     }
 }
--- src/adap-b04/names/StringName.ts
+++ src/adap-b04/names/StringName.ts
@@ -2,6 +2,10 @@ import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 import { AbstractName } from "./AbstractName";
 
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { MethodFailedException } from "../common/MethodFailedException";
+import { InvalidStateException } from "../common/InvalidStateException";
+
 export class StringName extends AbstractName {
 
     protected delimiter: string = DEFAULT_DELIMITER;
@@ -10,11 +14,35 @@ export class StringName extends AbstractName {
 
     constructor(source: string, delimiter?: string) {
         super();
+        IllegalArgumentException.assert(source !== null, "source is null");
+        IllegalArgumentException.assert(source !== undefined, "source is undefined");
+
         if (delimiter) {
+            IllegalArgumentException.assert(delimiter !== null, "delimiter is null");
+            IllegalArgumentException.assert(delimiter.length > 0, "delimiter is empty");
             this.delimiter = delimiter;
         }
         this.name = source;
         this.noComponents = 1;
+    }
+
+    protected assertClassInvariant(): void {
+        InvalidStateException.assert(
+            this.name !== null,
+            "name is null"
+        );
+        InvalidStateException.assert(
+            this.name !== undefined,
+            "name is undefined"
+        );
+        InvalidStateException.assert(
+            this.noComponents >= 0,
+            "noComponents is negative"
+        );
+        InvalidStateException.assert(
+            this.delimiter.length > 0,
+            "delimiter is empty"
+        );
     }
 
     public clone(): Name {
@@ -59,31 +87,98 @@ export class StringName extends AbstractName {
     }
 
     public getComponent(i: number): string {
-        return this.name;
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i < this.noComponents, "index out of bounds");
+        
+        const newName = this.name
+
+        MethodFailedException.assert(this.name !== null, "name is null");
+        MethodFailedException.assert(this.name !== undefined, "name is undefined");
+
+        return newName;
+
+
     }
 
     public setComponent(i: number, c: string) {
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i < this.noComponents, "index out of bounds");
+        IllegalArgumentException.assert(c !== null, "component is null");
+        IllegalArgumentException.assert(c !== undefined, "component is undefined");
+        IllegalArgumentException.assert(c.length > 0, "component is empty");
+
         this.name = c;
+        MethodFailedException.assert(this.name === c, "component not set correctly");
+        this.assertClassInvariant();
     }
 
     public insert(i: number, c: string) {
+        IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i <= this.noComponents, "index out of bounds");
+        IllegalArgumentException.assert(c !== null, "component is null");
+        IllegalArgumentException.assert(c !== undefined, "component is undefined");
+        IllegalArgumentException.assert(c.length > 0, "component is empty");
+        
+        const oldCount = this.noComponents;
+
         const components = this.name.split(this.delimiter);
         components.splice(i, 0, c);
         this.name = components.join(this.delimiter);
+        this.noComponents += 1;
+
+        MethodFailedException.assert(this.noComponents === oldCount + 1, "component not inserted correctly");
+
+        this.assertClassInvariant();
     }
 
     public append(c: string) {
+        IllegalArgumentException.assert(c !== null, "component is null");
+        IllegalArgumentException.assert(c !== undefined, "component is undefined");
+        IllegalArgumentException.assert(c.length > 0, "component is empty");
+
+        const oldCount = this.noComponents;
         this.name += this.delimiter + c;
+        this.noComponents += 1;
+
+        MethodFailedException.assert(this.noComponents === oldCount + 1, "component not appended correctly");
+
+        this.assertClassInvariant();
     }
 
     public remove(i: number) {
+         IllegalArgumentException.assert(i >= 0, "index is negative");
+        IllegalArgumentException.assert(i < this.noComponents, "index out of bounds");
+        IllegalArgumentException.assert(this.noComponents > 0, "cannot remove from empty name");
+
+        const oldCount = this.noComponents;
+
         const components = this.name.split(this.delimiter);
         components.splice(i, 1);
         this.name = components.join(this.delimiter);
+        
+        this.noComponents -= 1;
+
+        MethodFailedException.assert(this.noComponents === oldCount - 1, "component not removed correctly");
+
+        this.assertClassInvariant();
     }
 
     public concat(other: Name): void {
+        IllegalArgumentException.assert(other !== null, "other name is null");
+        IllegalArgumentException.assert(other !== undefined, "other name is undefined");
+
+        const oldCount = this.noComponents; 
+        const otherCount = other.getNoComponents();
+
         this.name += this.delimiter + other.asString();
+        this.noComponents += otherCount;
+
+        MethodFailedException.assert(
+            this.noComponents === oldCount + otherCount,
+            "noComponents not increased correctly"
+        );
+        
+        this.assertClassInvariant();
     }
 
 }
