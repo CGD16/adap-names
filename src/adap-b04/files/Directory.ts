88babc8aec3b0af847500afd27376e96bc8b7748
CGD16 <changgeng.drewes@gmail.com>
2025-12-01 15:23:09 +0100

    Files, Directory

--- src/adap-b04/files/Directory.ts
+++ src/adap-b04/files/Directory.ts
@@ -1,22 +1,49 @@
 import { Node } from "./Node";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
 
 export class Directory extends Node {
 
     protected childNodes: Set<Node> = new Set<Node>();
 
     constructor(bn: string, pn: Directory) {
+        IllegalArgumentException.assert(bn !== null, "baseName is null");
+        IllegalArgumentException.assert(bn !== undefined, "baseName is undefined");
+        IllegalArgumentException.assert(bn.length > 0, "baseName is empty");
+
+        IllegalArgumentException.assert(pn !== null, "parent is null");
+        IllegalArgumentException.assert(pn !== undefined, "parent is undefined");
+
         super(bn, pn);
     }
 
     public hasChildNode(cn: Node): boolean {
+        IllegalArgumentException.assert(cn !== null, "child node is null");
+        IllegalArgumentException.assert(cn !== undefined, "child node is undefined");
+
         return this.childNodes.has(cn);
     }
 
     public addChildNode(cn: Node): void {
+        IllegalArgumentException.assert(cn !== null, "child node is null");
+        IllegalArgumentException.assert(cn !== undefined, "child node is undefined");
+
+        IllegalArgumentException.assert(
+            !this.childNodes.has(cn),
+            "child node already exists"
+        );
+
         this.childNodes.add(cn);
     }
 
     public removeChildNode(cn: Node): void {
+        IllegalArgumentException.assert(cn !== null, "child node is null");
+        IllegalArgumentException.assert(cn !== undefined, "child node is undefined");
+
+        IllegalArgumentException.assert(
+            this.childNodes.has(cn),
+            "child node does not exist"
+        );
+        
         this.childNodes.delete(cn); // Yikes! Should have been called remove
     }
 
--- src/adap-b04/files/File.ts
+++ src/adap-b04/files/File.ts
@@ -1,6 +1,7 @@
 import { Node } from "./Node";
 import { Directory } from "./Directory";
 import { MethodFailedException } from "../common/MethodFailedException";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
 
 enum FileState {
     OPEN,
@@ -13,20 +14,60 @@ export class File extends Node {
     protected state: FileState = FileState.CLOSED;
 
     constructor(baseName: string, parent: Directory) {
+        IllegalArgumentException.assert(baseName !== null, "baseName is null");
+        IllegalArgumentException.assert(baseName !== undefined, "baseName is undefined");
+        IllegalArgumentException.assert(baseName.length > 0, "baseName is empty");
+
+        IllegalArgumentException.assert(parent !== null, "parent is null");
+        IllegalArgumentException.assert(parent !== undefined, "parent is undefined");
+        
         super(baseName, parent);
     }
 
     public open(): void {
-        // do something
+        IllegalArgumentException.assert(
+            this.state !== FileState.OPEN, 
+            "file is already open"
+        );
+        
+        IllegalArgumentException.assert(
+            this.state !== FileState.DELETED, 
+            "can't open deleted file"
+        );
+        
+        this.state = FileState.OPEN;
     }
 
     public read(noBytes: number): Int8Array {
-        // read something
+        IllegalArgumentException.assert(
+            this.state !== FileState.CLOSED, 
+            "cannot read from closed file"
+        );
+        
+        IllegalArgumentException.assert(
+            this.state !== FileState.DELETED, 
+            "cannot read from deleted file"
+        );
+        
+        IllegalArgumentException.assert(noBytes >= 0, "noBytes is negative");
+
         return new Int8Array();
     }
 
     public close(): void {
-        // do something
+        // PRECONDITION: Don't close a closed file
+        IllegalArgumentException.assert(
+            this.state !== FileState.CLOSED, 
+            "file is already closed"
+        );
+        
+        // PRECONDITION: Don't close a deleted file
+        IllegalArgumentException.assert(
+            this.state !== FileState.DELETED, 
+            "cannot close deleted file"
+        );
+        
+        this.state = FileState.CLOSED;
     }
 
     protected doGetFileState(): FileState {
